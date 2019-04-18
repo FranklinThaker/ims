dd0649f0c3c8652fead910fd49b461c413da2ad6
Franklin <franklin873@live.com>
2019-04-18 13:07:58 +0530

    Relationships have been removed for table AssignedComponent

--- server/models/index.js
+++ server/models/index.js
@@ -48,7 +48,7 @@ db.Categories = require('../models/categories.js')(sequelize, Sequelize)
 db.RequestComponents = require('../models/requestComponents.js')(sequelize, Sequelize)
 db.Incidents = require('../models/incidents.js')(sequelize, Sequelize)
 db.Components = require('../models/components.js')(sequelize, Sequelize);
-db.AssignedComponents = require('../models/assignedComponents.js')(sequelize, Sequelize);
+// db.AssignedComponents = require('../models/assignedComponents.js')(sequelize, Sequelize);
 db.IncidentUpdates = require('../models/incidentUpdates.js')(sequelize, Sequelize);
 
 
@@ -56,10 +56,10 @@ db.IncidentUpdates = require('../models/incidentUpdates.js')(sequelize, Sequeliz
 db.Components.belongsTo(db.Categories, {foreignKey: 'categoryId'});
 db.RequestComponents.belongsTo(db.Categories, { foreignKey: 'categoryId' })
 db.RequestComponents.belongsTo(db.Users, { foreignKey: 'userId' })
-db.AssignedComponents.belongsTo(db.Categories, {foreignKey: 'categoryId'});
-db.AssignedComponents.belongsTo(db.Components, {foreignKey: 'componentId'});
-db.AssignedComponents.belongsTo(db.Users, {as: 'AssignedBy',foreignKey: 'assignedBy'});
-db.AssignedComponents.belongsTo(db.Users, {as: 'AssignedTo', foreignKey: 'userId'});
+// db.AssignedComponents.belongsTo(db.Categories, {foreignKey: 'categoryId'});
+// db.AssignedComponents.belongsTo(db.Components, {foreignKey: 'componentId'});
+// db.AssignedComponents.belongsTo(db.Users, {as: 'AssignedBy',foreignKey: 'assignedBy'});
+// db.AssignedComponents.belongsTo(db.Users, {as: 'AssignedTo', foreignKey: 'userId'});
 db.Incidents.belongsTo(db.Users, {as: 'ResolvedBy', foreignKey: 'resolvedBy'});
 db.Incidents.belongsTo(db.Users, {as: 'IncidentBy', foreignKey: 'incidentBy'})
 db.Incidents.belongsTo(db.Users, { foreignKey: 'incidentBy' })
