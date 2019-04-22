f6b738bdc119c850a768c348c31ff34168dfb7ad
Franklin <franklin873@live.com>
2019-04-22 18:09:54 +0530

    Sidebar and navbar functionality added and routing added

--- client/src/Components/AdminHome.js
+++ client/src/Components/AdminHome.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import {Row, Col} from 'reactstrap';
 import AuthService from './AuthService';
 import withAuth from './withAuth';
 
@@ -10,6 +11,8 @@ import CategoryMaster from './CategoryMaster';
 import UserMaster from './UserMaster';
 import ComponentsMaster from './ComponentsMaster';
 import ManageComponents from './ManageComponents';
+import AdminSidebar from './AdminSidebar';
+import AdminNavbar from './AdminNavbar';
 
 const Auth = new AuthService();
 
@@ -28,19 +31,23 @@ class AdminHome extends Component {
 
   render() {
     return (
-      <>
-        <Navbar {...this.props} />
-        <div id='wrapper'>
-          <div id="content-wrapper" >
-            <p>{process.env.REACT_APP_SERVER}</p>
+      <>      
+        <BrowserRouter>
+        <>
+        <AdminNavbar {...this.props}/>
+        <Row className='rowNew'>          
+            <AdminSidebar />          
+          <Col className='rowNew'>
             <Route path='/admin/home/ViewProfile' render={() => (<ViewProfile {...this.props} />)} />
             <Route path='/admin/home/IncidentMaster' render={() => (<IncidentsMaster {...this.props} />)} />
             <Route path='/admin/home/CategoryMaster' render={() => (<CategoryMaster {...this.props} />)} />
             <Route path='/admin/home/UserMaster' render={() => (<UserMaster {...this.props} />)} />
             <Route path='/admin/home/ComponentMaster' render={() => (<ComponentsMaster {...this.props} />)} />
             <Route path='/admin/home/ManageComponents' render={() => (<ManageComponents {...this.props} />)} />
-            </div>
-        </div>
+          </Col>
+        </Row>
+        </>
+      </BrowserRouter>
 
       </>
     )
--- client/src/Components/AdminSidebar.js
+++ client/src/Components/AdminSidebar.js
@@ -1,44 +1,32 @@
 
-import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
+const AdminSidebar = () => {
+  return (
+    <>
+      <input type="checkbox" class="navBtn" id="navBtn" />
+      <nav>
+        <ul>
+          <li>
+            <Link to='/admin/home/ViewProfile'><i class="fa fa-user-circle"></i> View Profile</Link>
+          </li>
+          <li>
+            <Link to='/admin/home/IncidentMaster'><i class="fa fa-diamond"></i>Incidents</Link>
+          </li>
+          <li>
+            <Link to='/admin/home/CategoryMaster'><i class="fa fa-cube"></i>Categories</Link>
+          </li>
+          <li>
+            <Link to='/admin/home/ComponentMaster'><i class="fa fa-magic"></i>Components</Link>
+          </li>
+          <li>
+            <Link to='/admin/home/ManageComponents'><i class="fa fa-heart"></i>Manage Components</Link>
+          </li>
+        </ul>
+      </nav>
 
-class Sidebar extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOpen: false
-    }
-  }
-  openNav() {
-    this.setState({ isOpen: !this.state.isOpen })
-  }
-
-  closeNav() {
-    this.setState({ isOpen: false })
-  }
-  render() {
-    return (
-      <>
-        <div id="mySidenav" style={{ width: this.state.isOpen ? '250px' : '0px' }} className="sidenav">
-          <a href="javascript:void(0)" className="closebtn" onClick={() => this.openNav()}>&times;</a>
-          <Link to='' className="closebtn" onClick={() => this.openNav()}>&times; </Link>
-          <Link to='/admin/home/ViewProfile' onClick={() => this.closeNav()}>
-            <i className="fas fa-fw fa-tachometer-alt"></i>
-            View Profile
-          </Link>
-          <Link to='/admin/home/IncidentMaster' className="menu-item" onClick={() => this.closeNav()}>Incidents</Link>
-          <Link to='/admin/home/CategoryMaster' className="menu-item" onClick={() => this.closeNav()}>Categories</Link>
-          <Link to='/admin/home/UserMaster' className="menu-item" onClick={() => this.closeNav()}>Users</Link>
-          <Link to='/admin/home/ComponentMaster' className="menu-item" onClick={() => this.closeNav()}>Components</Link>
-          <Link to='/admin/home/ManageComponents' className="menu-item" onClick={() => this.closeNav()}>Manage Components</Link>
-        </div>
-        <span style={{ cursor: "pointer" }} className="openBtn" onClick={() => this.openNav()}>&#9776;</span>
-
-        
-      </>
-    );
-  }
+    </>
+  )
 }
-export default Sidebar;
+export default AdminSidebar;
 
